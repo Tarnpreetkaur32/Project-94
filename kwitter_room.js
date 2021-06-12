bf68943bf7199dba7bed17bba9ca5e988664bf9d
Tarnpreetkaur32 <79137457+Tarnpreetkaur32@users.noreply.github.com>
2021-06-12 12:34:41 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCt2u73fwhQYDCOosvA7XJsOG_jEAo0FSY",
+    authDomain: "kwitter-da854.firebaseapp.com",
+    databaseURL: "https://kwitter-da854-default-rtdb.firebaseio.com",
+    projectId: "kwitter-da854",
+    storageBucket: "kwitter-da854.appspot.com",
+    messagingSenderId: "1071920017500",
+    appId: "1:1071920017500:web:5ee1b203ddc72cd71e3699",
+    measurementId: "G-BR42J7LNZR"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
